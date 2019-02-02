0c3e3f69dd3a8fa38b53d8a650306eba157102db
Oleksii Domianich <43075229+GeFeSTTS@users.noreply.github.com>
2019-02-02 02:15:51 +0200

    var -> let (task2)

--- FLX_homework_6/homework/js/task2.js
+++ FLX_homework_6/homework/js/task2.js
@@ -1,10 +1,10 @@
-var amount = parseFloat(prompt("Please, write amount from 0 to 9999999"));
-var disc = parseFloat(prompt("Please, write discount from 0 to 99"));
+const amount = parseFloat(prompt("Please, write amount from 0 to 9999999"));
+const disc = parseFloat(prompt("Please, write discount from 0 to 99"));
 if (amount < 0 || amount > 9999999 || disc < 0 || disc > 99 || isNaN(amount) || isNaN(disc)) {
     alert("Invalid input data");
 } else {
-    var priceWithDiscount = amount - (amount * disc / 100);
-    var saved = amount - priceWithDiscount;
+    let priceWithDiscount = amount - (amount * disc / 100);
+    let saved = amount - priceWithDiscount;
     alert(`Price without discount: ${+amount.toFixed(2)} \nDiscount: ${+disc.toFixed(2)}%`
     + `\nPrice with discount: ${+priceWithDiscount.toFixed(2)} \nSaved: ${+saved.toFixed(2)}`);
 }
